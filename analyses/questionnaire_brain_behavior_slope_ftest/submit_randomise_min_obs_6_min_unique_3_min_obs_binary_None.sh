97369b15608f8fdfdd15849000c8e1f13a8a459e
Jaemon Jumpawong <jaemon@sh03-06n08.int>
2026-03-02 23:50:00 -0800

    ran ftest with corrected within subject slope maps and new exclusion criterias

---
+++ analyses/questionnaire_brain_behavior_slope_ftest/submit_randomise_min_obs_6_min_unique_3_min_obs_binary_None.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##
+#SBATCH -J brain_behavior_slope_ftest_min_obs_6_min_unique_3_min_obs_binary_None
+#SBATCH --time=36:00:00
+#SBATCH --begin=now
+#SBATCH -n 1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH -p russpold,hns,normal
+#SBATCH --output=/oak/stanford/groups/russpold/data/uh2/aim1/analysis_code/survey_medley_code/log/%x-%A-%a.out
+#SBATCH --error=/oak/stanford/groups/russpold/data/uh2/aim1/analysis_code/survey_medley_code/log/%x-%A-%a.err
+#SBATCH --mail-user=jaemon@stanford.edu
+#SBATCH --mail-type=FAIL
+# ------------------------------------------
+
+module load contribs 
+module load poldrack
+
+PROJECT_ROOT="/oak/stanford/groups/russpold/data/uh2/aim1/analysis_code/survey_medley_code"
+UV_SETUP="$PROJECT_ROOT/setup_uv_sherlock.sh"
+
+source $UV_SETUP
+
+/oak/stanford/groups/russpold/data/uh2/aim1/derivatives/survey_medley_results/questionnaire_brain_behavior_slope_ftest/output_glm_ftest_min_obs_6_min_unique_3_min_obs_binary_None/randomise_call.sh
